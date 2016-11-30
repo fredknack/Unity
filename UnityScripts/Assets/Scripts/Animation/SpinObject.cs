35535f32f04c3163eebdcfe31d4501d110e1008e
Fred Knack <fredknack@gmail.com>
2016-11-30 03:19:55 -0800

    Starting a script collection for Unity

---
+++ UnityScripts/Assets/Scripts/Animation/SpinObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+// Attach this to a gameObject to make it spin at a given speed
+public class SpinObject : MonoBehaviour
+{
+    public int speed = 50;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(speed * Time.deltaTime, 0, 0));
+    }
+}
