34598c8b222ab839cc744396d91d88ebe566c2d5
Fred Knack <info@fredknack.com>
2017-01-10 01:04:36 -0800

    Added simple mover

---
+++ UnityScripts/Assets/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+
+    public float MaxSpeed = 10f;
+    private Transform ThisTransform = null;
+
+    void Awake()
+    {
+        ThisTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        ThisTransform.position += ThisTransform.forward * MaxSpeed * Time.deltaTime;
+    }
+}
