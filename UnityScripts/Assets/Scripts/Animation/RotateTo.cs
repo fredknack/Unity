119d3c95113f0b187e9b6dd5771482240d71de51
Fred Knack <info@fredknack.com>
2017-01-18 03:29:01 -0800

    Quarternion & Gizmo

---
+++ UnityScripts/Assets/Scripts/Animation/RotateTo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTo : MonoBehaviour {
+
+    private Transform ThisTransform = null;
+    public float RotSpeed = 90f;
+
+	// Use this for initialization
+	void Awake () {
+	    ThisTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        ThisTransform.rotation *= Quaternion.AngleAxis(RotSpeed * Time.deltaTime, Vector3.up);
+        //ThisTransform.rotation = Quaternion.Euler(0f, 90f, 0f);
+    }
+}
