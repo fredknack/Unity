e128af4a524c9daf229acbcfc26ac4d52daa8042
Fred Knack <info@fredknack.com>
2022-05-19 15:41:18 -0700

    Slight refactor

--- player.cs
+++ player.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Apply this script to a capsule with a rigidbody (no gravity) and rotation constraint checkboxes selected.
-//Make a camera a child of the capsule. Make sure it is tagged as the Main Camera.
+//Apply this script to a capsule with a rigidbody (no gravity) and rotation constraint checkboxes selected
+//Make a camera a child of the capsule. Make sure it is tagged as the Main Camera
 
 public class Player : MonoBehaviour
 {
@@ -16,16 +16,15 @@ public class Player : MonoBehaviour
     public  float MaxLookX; //80
     public float minLookX; //-80
     private float rotX;
-    private Camera cam;
-    private Rigidbody rig;
+    private Camera _cam;
+    private Rigidbody _rig;
     
     void Awake()
     {
-        cam = Camera.main;
-        rig = GetComponent<Rigidbody>();
+        _cam = Camera.main;
+        _rig = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
     }
-
     void LateUpdate()
     {
         Move();
@@ -34,28 +33,24 @@ public class Player : MonoBehaviour
         }
         CamLook();
     }
-
     void Move() {
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
         Vector3 dir = transform.right * x + transform.forward * z;
-        dir.y = rig.velocity.y;
-        rig.velocity = dir;
+        dir.y = _rig.velocity.y;
+        _rig.velocity = dir;
     }
-
     void CamLook() {
         float y = Input.GetAxis("Mouse X") * lookSensitivity;
         rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
         rotX = Mathf.Clamp(rotX, minLookX, MaxLookX);
-        cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        _cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
         transform.eulerAngles += Vector3.up * y;
     }
-
     void TryJump() {
-        print("Jump");
         Ray ray = new Ray(transform.position, Vector3.down);
         if(Physics.Raycast(ray, 1.5f)){
-            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            _rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
