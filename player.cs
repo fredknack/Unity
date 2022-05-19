7ccf68bb3bea033e5bff16e2986c08a7828096e5
Fred Knack <info@fredknack.com>
2022-05-18 21:39:19 -0700

    Unity Player Controller

---
+++ player.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed; //6
+    public float jumpForce; //7
+
+    [Header ("Camera")]
+    public float lookSensitivity; //5
+    public  float MaxLookX; //80
+    public float minLookX; //-80
+    private float rotX;
+    private Camera cam;
+    private Rigidbody rig;
+    
+    void Awake()
+    {
+        cam = Camera.main;
+        rig = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        Move();
+        if(Input.GetButtonDown("Jump")){
+            TryJump();
+        }
+        CamLook();
+    }
+
+    void Move() {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir.y = rig.velocity.y;
+        rig.velocity = dir;
+    }
+
+    void CamLook() {
+        float y = Input.GetAxis("Mouse X") * lookSensitivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+        rotX = Mathf.Clamp(rotX, minLookX, MaxLookX);
+        cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
+    }
+
+    void TryJump() {
+        print("Jump");
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if(Physics.Raycast(ray, 1.5f)){
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
