9b1fa4f7f0ae0b8a293f32d5b442c21197b133e2
Fred Knack <info@fredknack.com>
2022-05-24 15:15:38 -0700

    Spawn Prefabs In Pool

---
+++ pooling/Spawner.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Spawner : MonoBehaviour
+{
+    
+    [SerializeField] private Bullet Prefab;
+
+    [SerializeField] private BoxCollider SpawnArea;
+    private ObjectPool<Bullet> BulletPool;
+    [SerializeField] ParticlePool _particlePool;
+    private void Start(){
+        InvokeRepeating(nameof(Spawn), 0.6f, 0.6f);
+    }
+
+    void Awake() {
+        BulletPool = new ObjectPool<Bullet>(CreatePooledObject, OnTakeFromPool, OnReturnToPool, OnDestroyObject, false, 10, 10);
+    }
+    private Bullet CreatePooledObject() {
+        Bullet instance = Instantiate(Prefab, Vector3.zero, Quaternion.identity);
+        instance.Disable += ReturnObjectToPool;
+        instance.gameObject.SetActive(false);
+
+        return instance;
+    }
+
+    private void ReturnObjectToPool(Bullet Instance) {
+        if(_particlePool != null && _particlePool.isActiveAndEnabled) {
+            _particlePool.Pool.Get().transform.position = Instance.transform.position;
+        }
+        BulletPool.Release(Instance);
+    }
+
+    private void OnTakeFromPool(Bullet Instance) {
+        Instance.gameObject.SetActive(true);
+        SpawnBullet(Instance);
+        Instance.transform.SetParent(transform, true);
+    }
+
+    private void OnReturnToPool(Bullet Instance) {
+        Instance.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyObject(Bullet Instance) {
+        Destroy(Instance.gameObject);
+    }
+
+    private void Spawn(){
+        BulletPool.Get();
+    }
+
+    private void SpawnBullet(Bullet instance) {
+        Vector3 spawnLocation = new Vector3(
+            SpawnArea.transform.position.x + SpawnArea.center.x + Random.Range(-4f * SpawnArea.bounds.extents.x, SpawnArea.bounds.extents.x ),
+            SpawnArea.transform.position.y + SpawnArea.center.y + Random.Range(-4f * SpawnArea.bounds.extents.x, SpawnArea.bounds.extents.y ),
+            SpawnArea.transform.position.z + SpawnArea.center.z + Random.Range(-4f * SpawnArea.bounds.extents.x, SpawnArea.bounds.extents.z )
+        );
+        instance.transform.position = spawnLocation;
+
+        instance.Shoot(spawnLocation, SpawnArea.transform.right, 6);
+    }
+}
