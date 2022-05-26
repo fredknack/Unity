c0329d16ae54d886644afbb52179136421c73dfc
Fred Knack <info@fredknack.com>
2022-05-25 20:42:42 -0700

    Move Object Towards Target

---
+++ movement/MoveObjectTowardsTarget.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectTowardsTarget : MonoBehaviour
+{
+    Vector3 targetForMovingObject = new Vector3(0,0,0);
+
+    [SerializeField]
+    private float moveObjectTowardsTargetSpeed;
+    void LateUpdate() {
+        MoveObjectAtConstantSpeedTowardsTarget();
+    }
+
+    private void MoveObjectAtConstantSpeedTowardsTarget(){
+        float distance = Vector3.Distance(targetForMovingObject, transform.position);
+
+        if(distance > 1.0f){
+            Vector3 dir = targetForMovingObject - transform.position;
+            dir.Normalize();
+            transform.position += dir * moveObjectTowardsTargetSpeed * Time.deltaTime;
+        }
+    }
+
+    
+}
