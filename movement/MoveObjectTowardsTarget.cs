f3aad96bbd5c260f44c95ce5a5f5d2e2ef0866d0
Fred Knack <info@fredknack.com>
2022-05-25 21:56:10 -0700

    Alternate Implementation

--- movement/MoveObjectTowardsTarget.cs
+++ movement/MoveObjectTowardsTarget.cs
@@ -1,26 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using UnityEngine;
 
 public class MoveObjectTowardsTarget : MonoBehaviour
 {
-    Vector3 targetForMovingObject = new Vector3(0,0,0);
+    // Vector3 targetForMovingObject = new Vector3(0,0,0);
+    //
+    // [SerializeField]
+    // private float moveObjectTowardsTargetSpeed;
+    // void LateUpdate() {
+    //     MoveObjectAtConstantSpeedTowardsTarget();
+    // }
+    //
+    // private void MoveObjectAtConstantSpeedTowardsTarget(){
+    //     float distance = Vector3.Distance(targetForMovingObject, transform.position);
+    //
+    //     if(distance > 1.0f){
+    //         Vector3 dir = targetForMovingObject - transform.position;
+    //         dir.Normalize();
+    //         transform.position += dir * moveObjectTowardsTargetSpeed * Time.deltaTime;
+    //     }
+    // }
+    [SerializeField] private Transform targetTransform;
 
-    [SerializeField]
-    private float moveObjectTowardsTargetSpeed;
-    void LateUpdate() {
-        MoveObjectAtConstantSpeedTowardsTarget();
-    }
+    private bool arrivedAtTarget = false;
 
-    private void MoveObjectAtConstantSpeedTowardsTarget(){
-        float distance = Vector3.Distance(targetForMovingObject, transform.position);
+    private void Update()
+    {
+        
+        if (arrivedAtTarget) return;
+        
+        Vector3 moveDir = (targetTransform.position - transform.position).normalized;
 
-        if(distance > 1.0f){
-            Vector3 dir = targetForMovingObject - transform.position;
-            dir.Normalize();
-            transform.position += dir * moveObjectTowardsTargetSpeed * Time.deltaTime;
+        float moveSpeed = 10f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, targetTransform.position) < .5f)
+        {
+            arrivedAtTarget = true;
+            Debug.Log("Arrived at Target!");
         }
     }
 
-    
 }
